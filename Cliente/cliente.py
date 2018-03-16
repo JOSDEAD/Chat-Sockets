749f49568b3635d683294a560236afccc9df5785
Unknown <josdead@estudiantec.cr>
2018-03-16 11:47:51 -0600

    hilos

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -1,13 +1,51 @@
+import sys
 import socket
+import select
 
-TCP_IP = 'localhost'
-TCP_PORT = 5000
-BUFFER_SIZE = 1024
-MESSAGE = str.encode( "Hello, World!")
+from threading import *
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
-print("received data: "+ data.decode())
+from socket import *
+import _thread, time
+import msvcrt as m
+
+
+def recvMsg(sock):
+    while True:
+        recvmsg = sock.recv(1024)
+        print(
+        '<Server>>> ' + recvmsg.decode())
+
+
+if __name__ == '__main__':
+
+    host = "localhost"
+    port = 5000
+
+    try:
+        s = socket(AF_INET, SOCK_STREAM)
+        s.connect((host, port))
+
+        _thread.start_new_thread(recvMsg, (s,))
+
+        time.sleep(1)
+        nickmsg = input('My Nickname: ')
+        s.send(nickmsg)
+
+        time.sleep(2)
+        print(
+        'Wait!...')
+
+        while True:
+            if m.getch() != '\r':
+                continue
+            sendmsg = input(' - Send: ')
+            if sendmsg == 'exit()':
+                break
+            s.send(sendmsg)
+
+        s.close()
+    except:
+        print(
+        'Wrong address!')
+
+    input('Exit client (Press any key!)')
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,20 +1,41 @@
 import socket
-
-HOST='localhost'
+import select
+HOST=''
 PORT=5000
-BUFFER_SIZE = 20
+BUFFER_SIZE = 1024
+SOCKET_LIST=[]
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((HOST,PORT))
-s.listen(1)
 
-conn,addr=s.accept()
+servidor_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+servidor_socket.bind((HOST,PORT))
+servidor_socket.listen(10)
 
-print("Connection address", addr)
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    decodedData=data.decode()
-    print("received data: "+ decodedData)
-    conn.send(data)  # echo
-conn.close()
+SOCKET_LIST.append(servidor_socket)
+
+#Empieza el chat
+
+while True:
+    ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [], 0)
+    for sockets in ready_to_read:
+        #Solicitud de nueva coneccion
+        if sockets == servidor_socket:
+            newSocket,addr= servidor_socket.accept()
+            SOCKET_LIST.append(newSocket)
+            print("Cliente (%s, %s) connectado" % addr)
+        else:
+            try:
+                data= sockets.recv(BUFFER_SIZE)
+                if data:
+                    print("["+str(addr)+"]: "+data.decode())
+                    for rectores in SOCKET_LIST:
+                        if rectores != sockets and rectores != servidor_socket:
+                            print(rectores)
+                            rectores.send(data)
+                else:
+                    if sockets in SOCKET_LIST:
+                        SOCKET_LIST.remove(sockets)
+            except:
+                continue
+
+servidor_socket.close()
+
