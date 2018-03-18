af8aa00c166f29f05b5bbfc2c2dee3e7858530fb
Unknown <josdead@estudiantec.cr>
2018-03-17 18:27:15 -0600

    Peticiones de Hora

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -8,45 +8,44 @@ def recibirMsg(sock):
     while True:
         data = sock.recv(4096)
         if not data:
-            print(
-                '\nDisconnected from chat server')
+            print('\nTe has desconectado del servidor')
             sys.exit()
         else:
             # print data
             sys.stdout.write(data.decode())
-            sys.stdout.write('[Me] ');
+            sys.stdout.write('[Yo] ');
             sys.stdout.flush()
+
 
 def chat_client():
 
-
-    host = "localhost"
-    port = 5000
-
+    host = input("Escriba el IP(default=localhost):")
+    port = input("Escriba el Puerto(default=5000):")
+    host = host if (len(host) > 0) else 'localhost'
+    port = int(port) if (len(port) > 0) else 5000
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-    # connect to remote host
+    # conectar con el servidor
     try:
         s.connect((host, port))
     except:
-        print(
-        'Unable to connect')
+        print('\nImposible conectar')
         sys.exit()
 
+    #hilo para empezar a recibir mensajes
     _thread.start_new_thread(recibirMsg, (s,))
 
-    print(
-    'Connected to remote host. You can start sending messages')
-    sys.stdout.write('[Me] ');
+    print('\nSe ha establecido conexion')
+    sys.stdout.write('[Yo] ');
     sys.stdout.flush()
 
-
+    #ciclo para escribir mensajes
     while 1:
         # user entered a message
         msg = sys.stdin.readline()
         s.send(str.encode(msg))
-        sys.stdout.write('[Me] ');
+        sys.stdout.write('[Yo] ');
         sys.stdout.flush()
 
 
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,29 +1,62 @@
 import socket
 import select
+import urllib.request
+import json
 HOST=''
 PORT=5000
 BUFFER_SIZE = 1024
 SOCKET_LIST=[]
 
+#Obtener lista de paises y sus zonas horarias
+response = urllib.request.Request('http://api.timezonedb.com/v2/list-time-zone?key=07T61XN44HZU&format=json')
+r = urllib.request.urlopen(response).read()
+jsonPaises = r.decode('utf8').replace("'", '"')
+# Load the JSON to a Python list & dump it back out as formatted JSON
+paises = json.loads(jsonPaises)
 
+
+
+""""port = input("Escriba el Puerto(default=5000):")
+port = int(port) if (len(port) > 0) else 5000"""""
 servidor_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 servidor_socket.bind((HOST,PORT))
 servidor_socket.listen(10)
 
 SOCKET_LIST.append(servidor_socket)
 
+
+
+##Obtener hora de algun pais
+def obtenerHora(server_socket,pais):
+    pais = pais.replace("\n", '')
+    zonename = ""
+    for i in paises["zones"]:
+        if i["countryName"] == pais:
+            zonename = i["zoneName"]
+            break;
+
+    if zonename != "":
+        url = 'http://api.timezonedb.com/v2/get-time-zone?key=07T61XN44HZU&by=zone&format=json&zone=' + zonename
+        response = urllib.request.Request(url)
+        r = urllib.request.urlopen(response).read()
+        my_json = r.decode('utf8').replace("'", '"')
+        data = json.loads(my_json)
+        broadcast(server_socket,server_socket,"\r[Server] Hora en "+pais+' '+data["formatted"]+'\n')
+    else:
+        broadcast(server_socket, server_socket, "\r[Server] Pais no encontrado\n")
+
+
+
 #Empieza el chat
-# broadcast chat messages to all connected clients
+# envia mensajes a todos los clientes, menos al que envia
 def broadcast(server_socket, sock, message):
     for socket in SOCKET_LIST:
-        # send the message only to peer
+        # enviar solo a receptores
         if socket != server_socket and socket != sock:
             try:
                 socket.send(str.encode(message))
             except:
-                # broken socket connection
                 socket.close()
-                # broken socket, remove it
                 if socket in SOCKET_LIST:
                     SOCKET_LIST.remove(socket)
 
@@ -36,18 +69,27 @@ while True:
             newSocket,addr= servidor_socket.accept()
             SOCKET_LIST.append(newSocket)
             print("Cliente (%s, %s) connectado" % addr)
+            broadcast(servidor_socket, sockets, "[%s:%s] entro al chat\n" % addr)
         else:
             try:
                 data= sockets.recv(BUFFER_SIZE)
                 if data:
-                    broadcast(servidor_socket,sockets,"\r"+'[' + str(sockets.getpeername()) + ']'+data.decode())
+                    broadcast(servidor_socket, sockets, "\r" + '[' + str(sockets.getpeername()) + ']' + data.decode())
+                    decodedData = data.decode()
+                    if "@" == decodedData[0]:
+                        if "@hora " in decodedData:
+                            pais=decodedData.replace("@hora ", '')
+                            obtenerHora(servidor_socket,pais)
+
+
+
                 else:
                     if sockets in SOCKET_LIST:
                         SOCKET_LIST.remove(sockets)
                     broadcast(servidor_socket, sockets, "Cliente (%s, %s) se desconecto\n" % addr)
 
             except:
-                ##broadcast(servidor_socket, sockets, "Cliente (%s, %s) se desconecto\n" % addr)
+
                 continue
 
 
